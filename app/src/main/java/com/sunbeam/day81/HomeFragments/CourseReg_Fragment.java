c84b76ed1db1506692b8996e651225b632bc73ce
pratikthebos <pratikvkambale@53gmail.com>
2025-02-27 12:10:38 +0530

    changes

--- app/src/main/java/com/sunbeam/day81/HomeFragments/CourseReg_Fragment.java
+++ app/src/main/java/com/sunbeam/day81/HomeFragments/CourseReg_Fragment.java
@@ -18,7 +18,7 @@ import com.sunbeam.day81.R;
 public class CourseReg_Fragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
